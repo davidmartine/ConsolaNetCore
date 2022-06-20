f450ff6eb33d517769a635ffee8fef215f41dbba
David <juanda.mg@hotmail.com>
2022-06-19 21:42:21 -0500

    Nueva Codifocacion

--- ConsolaNetCore/Program.cs
+++ ConsolaNetCore/Program.cs
@@ -7,7 +7,9 @@ namespace ConsolaNetCore
         static void Main(string[] args)
         {
             decimal dato = 2.7m;
-            Console.WriteLine("Hola primera linea net core {0}",dato);
+            decimal dato2 = 2.5m;
+            decimal resultado = dato + dato2;
+            Console.WriteLine("Hola primera linea net core {0}",resultado);
             Console.ReadKey();
 
         }
