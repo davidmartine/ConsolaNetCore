bc64452bdedaf7ff50c32a24ae2eb45ac4288cea
David <juanda.mg@hotmail.com>
2022-06-19 20:12:06 -0500

    Agregar archivos de proyecto.

---
+++ ConsolaNetCore/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ConsolaNetCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal dato = 2.7m;
+
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+
+        }
+    }
+}
