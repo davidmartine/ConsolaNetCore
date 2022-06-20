fae2478259b2ef2904b6f79c5accbfe316e54193
davidmartine <44732199+davidmartine@users.noreply.github.com>
2022-06-19 22:00:49 -0500

    pruebas desde github

--- ConsolaNetCore/Program.cs
+++ ConsolaNetCore/Program.cs
@@ -9,6 +9,7 @@ namespace ConsolaNetCore
             decimal dato = 2.7m;
             decimal dato2 = 2.5m;
             decimal resultado = dato + dato2;
+            decimal total=resultado+3.6;
             Console.WriteLine("Hola primera linea net core {0}",resultado);
             Console.ReadKey();
 
