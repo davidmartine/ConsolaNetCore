f42009db0ece00abe01e719776c519a0eb48207d
David <juanda.mg@hotmail.com>
2022-06-19 20:58:36 -0500

    Primer Cambio Nueva Clase

---
+++ ConsolaNetCore/ClasePruebas.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsolaNetCore
+{
+    public class ClasePruebas
+    {
+        public int numeros { get; set; }
+    }
+}
