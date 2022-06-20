a31a6a2ec6bf307e75c4117c170ae33c853fd4ab
David <juanda.mg@hotmail.com>
2022-06-19 22:19:40 -0500

    Cabios Calculador

--- ConsolaNetCore/Program.cs
+++ ConsolaNetCore/Program.cs
@@ -9,8 +9,8 @@ namespace ConsolaNetCore
             decimal dato = 2.7m;
             decimal dato2 = 2.5m;
             decimal resultado = dato + dato2;
-            decimal total=resultado+3.6;
-            Console.WriteLine("Hola primera linea net core {0}",resultado);
+            decimal total=resultado+3.6m;
+            Console.WriteLine("Hola primera linea net core {0}",total);
             Console.ReadKey();
 
         }
