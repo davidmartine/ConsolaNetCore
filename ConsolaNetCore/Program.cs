43dfa93736246724cfe525bd43b685c15fce2cb9
David <juanda.mg@hotmail.com>
2022-06-19 20:27:27 -0500

    Primer cambio

--- ConsolaNetCore/Program.cs
+++ ConsolaNetCore/Program.cs
@@ -7,8 +7,7 @@ namespace ConsolaNetCore
         static void Main(string[] args)
         {
             decimal dato = 2.7m;
-
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hola primera linea net core {0}",dato);
             Console.ReadKey();
 
         }
